1c2128acfae4456b549c7f7eaf760db4406e8d11
Clatirissa Batista <clarissapbatista@gmail.com>
2023-05-08 20:09:19 +0100

    returning to the Modern React with Redux course

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(el);
 
 // 4. Create a component
 function App() {
-  return <h1>Hi there!</h1>;
+  return <h1>My React app!</h1>;
 }
 
 // 5. Show the component on the screen
