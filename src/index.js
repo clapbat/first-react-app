12db12850e36b089413b1fb3ff00398b6a33aae0
Clatirissa Batista <clarissapbatista@gmail.com>
2023-05-08 21:03:24 +0100

    nothing new

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ const root = ReactDOM.createRoot(el);
 
 // 4. Create a component
 function App() {
-  return <h1>My React app!</h1>;
+  return (
+    <input placeholder="hi there" />
+  );
 }
 
 // 5. Show the component on the screen
